88fdd22968aafcb81703c12423bb4c73bba92d07
Tnsnh2422 <118276610+Tnsnh2422@users.noreply.github.com>
2022-12-06 20:29:39 +0700

    Updated
    Ẩn file thừa

---
+++ QuanLyThuVien/src/Model/sinhVienTableModel.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/*package Model;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.table.AbstractTableModel;
+
+public class sinhVienTableModel extends  AbstractTableModel{
+    private final String[] tenCot = {"TÊN ĐĂNG NHẬP", "MẬT KHẨU", "HỌ TÊN","TUỔI","GIỚI TÍNH"};
+    private List<sinhVien> sinhVien;
+
+    public sinhVienTableModel() {
+        this.sinhVien = new ArrayList<>();
+    }
+    
+    public void setData(List<sinhVien> sinhVien) {
+        this.sinhVien = sinhVien;
+
+        // Mỗi khi dữ liệu của tableModel được cập nhật qua hàm setData thì 
+        // tableModel sẽ thông báo cho cái table trong View cập lại
+        fireTableDataChanged();
+    }
+
+    @Override
+    public String getColumnName(int i) {
+        return tenCot[i];
+    }
+    
+    @Override
+    public int getRowCount() {
+        return sinhVien.size();
+    }
+
+    @Override
+    public int getColumnCount() {
+        return tenCot.length;
+    }
+
+    @Override
+    public Object getValueAt(int hang, int cot) {
+        sinhVien sv = sinhVien.get(hang);
+        switch(cot){
+            case 0:
+                return sv.getTenDangNhap();
+            case 1: 
+                return sv.getMatKhau();
+            case 2: 
+                return sv.getHoTen();
+            case 3: 
+                return sv.getTuoi();
+            case 4:
+                 return sv.getGioiTinh();
+            
+        }
+        return null ;
+    }
+    
+}
+*/
---
+++ QuanLyThuVien/test/main.java
@@ -0,0 +1,21 @@
+
+import Controller.controllerLOGIN;
+import View.formLOGIN;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+public class main {
+    public static void main(String[] args) {
+         java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                formLOGIN form = new formLOGIN();
+                     controllerLOGIN controLOGIN = new controllerLOGIN(form);
+                     controLOGIN.showFormLogin();   
+            }
+        });
+    }
+}
